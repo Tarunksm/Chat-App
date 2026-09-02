1abc8489aec8ff2199a90ee2c3a74379f1fb4d6e
Tarun <tarunksm.07@gmail.com>
2026-09-02 17:37:22 +0530

    configure better auth client too

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+import "dotenv/config.js";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import prisma from "./prisma.js";
@@ -5,6 +6,18 @@ import prisma from "./prisma.js";
 const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    github: {
+      enabled: true,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
 
 export default auth;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
-console.log(process.env.DATABASE_URL);
 
 const prisma = new PrismaClient({ adapter });
 
